185c4f88ea5971f23b3b2021ea2f02733b9518ce
nabiCoder <nabiulli_n@vk.com>
2024-05-11 17:47:28 +0500

    Added new base colors

---
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/AppColors.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+enum AppColors {
+   
+    static let mainBlackColor = UIColor.init(hexString: "010F07")
+    static let activeOrangeColor = UIColor.init(hexString: "EEA734")
+    static let accentOrangeColor = UIColor.init(hexString: "F8B64C")
+    static let badyTextGreyColor = UIColor.init(hexString: "868686")
+    static let backgroundWhiteColor = UIColor.init(hexString: "FFFFFF")
+    static let greenColor = UIColor.init(hexString: "22A45D")
+}
--- FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -4,9 +4,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = AppColors.activeOrangeColor
     }
-
-
 }
-
